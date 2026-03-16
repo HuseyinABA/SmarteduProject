63f7332662fe8b0996ad6bf7a35f7a1ce4c3c914
Hüseyin ABA <abahuseyin992@gmail.com>
2026-03-16 12:39:37 +0300

    setup mongodb connection and create course, category models with slugify

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 
-//TEMPLATE ENGINE
+// DATABASE CONNECTION
+mongoose.connect('mongodb://127.0.0.1/smartedu-db')
+  .then(() => {
+    console.log("DB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("DB Connection Error:", err);
+  });
+
+// TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
-//Middlewares
+// MIDDLEWARES
 app.use(express.static("public"));
+app.use(express.json()); // For parsing application/json (Postman)
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
-//ROUTES
+// ROUTES
 app.use('/', pageRoute);
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
